e864fbf31aae64d8ff101e4358b6802749788f60
anshikpath <pramodpathak7ip@gmail.com>
2025-06-29 15:40:21 +0530

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,1 @@
+function press(val) { document.getElementById('display').value += val; } function calculate() { try { document.getElementById('display').value = eval(document.getElementById('display').value); } catch (e) { alert('Invalid Expression'); } } function clearDisplay() { document.getElementById('display').value = ''; } 
